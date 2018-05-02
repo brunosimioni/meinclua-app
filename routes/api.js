4fe240040ed7bd3bdc5630ab62bacc76c477c8d5
Leandro Oliveira <lelemarcelo87@gmail.com>
2018-05-01 22:20:24 -0300

    asd

--- routes/api.js
+++ routes/api.js
@@ -22,23 +22,49 @@ GooglePlaceAPIDetails += "key=" + apiKey;
 
 router.get('/places', function(req, res) {
 
-//[todo] diferenciar o que não está no banco, com uma cor diferente
-
   unirest.get(GooglePlaceAPISearch)
     .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
     .end(function (response) {
-      res.json(response.body);
+
+      var places = response.body.results;
+
+      db.query("SELECT * FROM escolas").then(result => {
+
+        var escolas = result.rows;
+        for (var i = 0; i < places.length; i++) {
+          for (var j = 0; j < escolas.length; j++) {
+            if (escolas[j].escola_endereco_gmaps_id == places[i].id) {
+              places[i].escolaData = escolas[j];
+            }
+          }
+        }
+
+        res.json(places);
+
+      }).catch(e => console.error(e.stack));
     });
 });
 
-router.get("/places/details/:id", function (req, res){
+router.get("/places/details/:id/gmapsid/:gmapsid", function (req, res){
 
   var placeId = req.params.id;
+  var gmapsId = req.params.gmapsid;
   var reqUrl = GooglePlaceAPIDetails.replace("__placeid__", placeId);
   unirest.get(reqUrl)
     .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
     .end(function (response) {
-      res.json(response.body);
+
+      db.query("SELECT avg(nps) FROM escolas_nps WHERE gmaps_id = $1", [gmapsId]).then(result => {
+
+        var avg_score = result.rows[0].avg;
+
+        response.body.npsData = {
+            avg_score: avg_score
+        }
+
+        res.json(response.body);
+
+      }).catch(e => console.error(e.stack));
     });
 });
 
@@ -75,7 +101,7 @@ router.post("/signup", function(req, res) {
       var query = "INSERT INTO escolas (" + fields.join(", ");
       query += ") VALUES (";
       for (var i = 0; i < fields.length; i++) {
-        query += "?"
+        query += ("$"+(i+1))
 
         if (i < fields.length-1)
           query += ","
@@ -84,16 +110,13 @@ router.post("/signup", function(req, res) {
       query += ")";
 
       console.log("signup escola query: " + query);
-      stmt = db.prepare(query, params);
+      db.query(query, params).then(result => {}).catch(e => console.error(e.stack));
     }
     else if (data.type == "usuario")
     {
       console.log("insercao no banco");
-      stmt = db.prepare("INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)", data.usuario_nome, data.usuario_email, data.usuario_senha);
+      db.query("INSERT INTO usuarios (nome, email, senha) VALUES ($1, $2, $3)", [data.usuario_nome, data.usuario_email, data.usuario_senha]).then(result => {}).catch(e => console.error(e.stack));;
     }
-
-    stmt.run();
-    stmt.finalize();
 
     req.session.user = data;
     res.json({user: data, status: "ok"});
@@ -109,25 +132,34 @@ router.post("/login", function(req, res) {
       var email = data.login_email;
       var senha = data.login_senha;
 
-      db.get("SELECT * FROM usuarios WHERE email = ? and senha = ?", email, senha, function(err, usuario)
+      db.query("SELECT * FROM usuarios WHERE email = $1 AND senha = $2", [email, senha]).then(result =>
       {
+        var usuario = result.rows[0];
         console.log("Tentativa de login de usuário. Email: " + email + " + senha: " + senha + " resultado: " + JSON.stringify(usuario));
-        if (usuario == null) {
-          db.get("SELECT * FROM escolas WHERE email = ? and senha = ?", email, senha, function(err, escola) {
+
+        if (usuario == null)
+        {
+          db.query("SELECT * FROM escolas WHERE escola_email = $1 and escola_senha = $2", [email, senha]).then(result => {
+
+            var escola = result.rows[0];
+
             console.log("Tentativa de login de escola. Email: " + email + " + senha: " + senha + " resultado: " + JSON.stringify(escola));
+
             if (escola == null) {
                 res.json({escola: escola, status: "nok"});
-            } else {
+            }
+            else
+            {
               // deixar o dado parecido com o form de signup
               for (var i in escola) {
                 escola["escola_"+i] = escola[i];
                 delete escola[i];
               }
-              usuario.type = "escola";
+              escola.type = "escola";
               req.session.user = escola;
               res.json({status: "ok"});
             }
-          });
+          }).catch(e => console.error(e.stack));
         }
         else
         {
@@ -140,7 +172,8 @@ router.post("/login", function(req, res) {
           req.session.user = usuario;
           res.json({status: "ok"});
         }
-     });
+
+      }).catch(e => console.error(e.stack));
   }
   catch(e) {
     console.log(e)
@@ -158,10 +191,11 @@ router.post("/rating", function(req, res) {
   var value = data.value;
   var place = data.place;
 
+  if (!place.id)
+    res.json({status: "not_found"});
+
   console.log("votação, nota: " + value + " escola:" + JSON.stringify(place));
-  var stmt = db.prepare("INSERT INTO escolas_nps (id_escola, nps) VALUES (?, ?)", place.id, value);
-  stmt.run();
-  stmt.finalize();
+  db.query("INSERT INTO escolas_nps (gmaps_id, nps) VALUES ($1, $2)", [place.id, value]).then(result => {}).catch(e => console.error(e.stack));
 
   res.json({status: "ok"});
 });
--- shared/Database.js
+++ shared/Database.js
@@ -1,24 +1,40 @@
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
-console.log("Initializing database...")
+const { Client } = require('pg')
+const client = new Client()
+
+console.log("Connecting to PostgreSQL...")
+client.connect();
 
 try {
-
-  db.serialize(function() {
-    db.run("CREATE TABLE email_contato (sender TEXT, text TEXT)");
-    db.run("CREATE TABLE usuarios (id INTEGER, nome TEXT, email TEXT, senha TEXT)");
-    db.run("CREATE TABLE escolas (id INTEGER, nps_avg_score INTEGER, escola_email TEXT, escola_senha TEXT, escola_cnpj TEXT, escola_telefone TEXT, escola_especialidade TEXT, escola_numalunos TEXT, escola_cid TEXT, escola_endereco_completo TEXT, escola_endereco_route TEXT, escola_endereco_street_number TEXT, escola_endereco_complemento TEXT, escola_endereco_administrative_area_level_2 TEXT, escola_endereco_administrative_area_level_1 TEXT, escola_endereco_postal_code TEXT, escola_endereco_country TEXT, escola_endereco_gmaps_id, TEXT, escola_check_estrutura TEXT, escola_check_vagas TEXT, escola_check_integral TEXT, escola_professores TEXT, escola_check_termos TEXT)");
-    db.run("CREATE TABLE escolas_nps (id_escola INTEGER, nps INTEGER, FOREIGN KEY (id_escola) REFERENCES escolas(id))");
-
-    db.run("INSERT INTO usuarios VALUES (1, 'User1', 'user1@users.com', 'user')");
-  });
+  runCreateQuery("CREATE TABLE IF NOT EXISTS usuarios (id BIGSERIAL PRIMARY KEY, nome TEXT, email VARCHAR, senha VARCHAR)");
+  runCreateQuery("CREATE TABLE IF NOT EXISTS escolas (" +
+      "id BIGSERIAL PRIMARY KEY, escola_email VARCHAR, escola_senha VARCHAR, escola_cnpj TEXT, " +
+      "escola_telefone TEXT, escola_especialidade TEXT, escola_numalunos TEXT, escola_cid TEXT, " +
+      "escola_endereco_completo TEXT, escola_endereco_route TEXT, escola_endereco_street_number TEXT, " +
+      "escola_endereco_complemento TEXT, escola_endereco_administrative_area_level_2 TEXT, " +
+      "escola_endereco_administrative_area_level_1 TEXT, escola_endereco_postal_code TEXT, " +
+      "escola_endereco_country TEXT, escola_endereco_gmaps_id TEXT, escola_check_estrutura TEXT, " +
+      "escola_check_vagas TEXT, escola_check_integral TEXT, escola_professores TEXT, escola_check_termos TEXT)");
+  runCreateQuery("CREATE TABLE IF NOT EXISTS escolas_nps (ID BIGSERIAL PRIMARY KEY, gmaps_id VARCHAR, nps INTEGER)");
+  runCreateQuery("INSERT INTO usuarios VALUES (1, 'User1', 'user1@users.com', 'user')");
+  runCreateQuery("INSERT INTO public.escolas (" +
+	   "id, escola_email, escola_senha, " +
+     "escola_cnpj, escola_telefone, escola_especialidade, " +
+     "escola_numalunos, escola_cid, escola_endereco_completo, " +
+     "escola_endereco_route, escola_endereco_street_number, escola_endereco_complemento, " +
+     "escola_endereco_administrative_area_level_2, escola_endereco_administrative_area_level_1, " +
+     "escola_endereco_postal_code, escola_endereco_country, escola_endereco_gmaps_id, " +
+     "escola_check_estrutura, escola_check_vagas, escola_check_integral, " +
+     "escola_professores, escola_check_termos) " +
+	   " VALUES (1, 'escola@escola', 'escola', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');");
 }
 catch(e) {
   console.log(e);
 }
 
-var database = {
-  db: db,
-}
+function runCreateQuery(query) {
+  client.query(query)
+    .then(res => {})
+    .catch(e => console.error(e.stack))
+};
 
-module.exports = db;
+module.exports = client;
